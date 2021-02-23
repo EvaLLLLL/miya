c957746f31869af80ebbc773e49a20e78eb9b96c
lyhhh <17610603221@163.com>
2021-02-23 18:06:53 +0800

    add antd-mobile

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button } from 'antd-mobile'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button>Click me</Button>
     </View>
   )
 }
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'import',
+        {
+          libraryName: 'antd-mobile',
+          style: 'css',
+        },
+      ],
+    ],
+  }
+}
